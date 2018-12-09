29f8bc094046e143b6f0a04503e223cb3cf21e8a
williansugden <sugdenb@hotmail.com>
2018-12-09 13:29:38 -0800

    added functions to php dec9

--- wp-content/plugins/gutenberg/gutenberg.php
+++ wp-content/plugins/gutenberg/gutenberg.php
@@ -3,15 +3,15 @@
  * Plugin Name: Gutenberg
  * Plugin URI: https://github.com/WordPress/gutenberg
  * Description: Printing since 1440. This is the development plugin for the new block editor in core.
- * Version: 4.6.0
+ * Version: 4.6.1
  * Author: Gutenberg Team
  *
  * @package gutenberg
  */
 
 ### BEGIN AUTO-GENERATED DEFINES
-define( 'GUTENBERG_VERSION', '4.6.0' );
-define( 'GUTENBERG_GIT_COMMIT', '7d2c994e7915dfc4b919ce4e5782d0091fc4c13b' );
+define( 'GUTENBERG_VERSION', '4.6.1' );
+define( 'GUTENBERG_GIT_COMMIT', '7e27b6438841d57ef71b8897ad2a205ec78166e1' );
 ### END AUTO-GENERATED DEFINES
 
 gutenberg_pre_init();
--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -7,3 +7,74 @@ function theme_enqueue_styles() {
        array('parent-style')
    );
 }
+
+
+//removes the wp meta gen
+remove_action('wp_head','wp_generator');
+
+
+/*
+//Add dashdoard widget did not work 
+function sugden_dashboard_widgets(){
+global $wp_meta_boxes;
+wp_add_dashboard_widget('sugden_help_widget','SUGDEN-Lets Make Some WordPress!','sugden_dashboaed_help');
+}
+function sugden_dashboard_help(){
+    echo'<p>Need some help with WordPress Site? - Email me <a
+    href="mailto:billsugden@billsugden.com">
+    here</a>.And check out my website here:
+    <a href="http://www.billsugden.com"target="_blank">SUGDEN</a></p>';
+}
+add_action('wp_dashboard_setup',sugden_daskboard_widgets');
+*/
+
+//Add Custom Dashboard Widgets in WordPress
+add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+ 
+function my_custom_dashboard_widgets() {
+global $wp_meta_boxes;
+ 
+wp_add_dashboard_widget('custom_help_widget', 'Theme Support', 'custom_dashboard_help');
+}
+ 
+function custom_dashboard_help() {
+echo '<p>Welcome to your Custom Profile Theme! Need help? Contact the  developer Bill Sugden <a href="mailto:billsugden@billsugden.com">here</a>. For our website visit: <a href="http://www.billsugden.com" target="_blank">billsugden.com</a></p>';
+}
+
+
+//Change the Footer in WordPress Admin Panel
+
+function remove_footer_admin () {
+ 
+echo ' Page designed by <a href="http://www.billsugden.com" target="_blank">billsugden.com</a> | WordPress page designer: <a href="https://www.billsugden.com" target="_blank">Bill Sugden</a></p>';
+ 
+}
+ 
+add_filter('admin_footer_text', 'remove_footer_admin');
+
+
+// custom logo 
+/*
+
+function wpb_custom_logo() {
+echo '
+<style type="text/css">
+#wpadminbar #wp-admin-bar-wp-logo > .ab-item .ab-icon:before {
+background-image: url(' . get_bloginfo('stylesheet_directory') . '/images/custom-logo.png) !important;
+background-position: 0 0;
+color:rgba(0, 0, 0, 0);
+}
+#wpadminbar #wp-admin-bar-wp-logo.hover > .ab-item .ab-icon {
+background-position: 0 0;
+}
+</style>
+';
+}
+//hook into the administrative header output
+add_action('wp_before_admin_bar_render', 'wpb_custom_logo');
+*/
+
+
+// remove wellcome dashboard
+remove_action('welcome_panel', 'wp_welcome_panel');
+
